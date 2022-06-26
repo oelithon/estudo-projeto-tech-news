f036f1e4b19eca03fb0b78d335ebcfd5c572bd81
Elithon Silva <elithonsilva@gmail.com>
2022-06-26 11:21:00 -0300

    implementa a função scrape_novidades

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -18,8 +19,13 @@ def fetch(url):
 
 
 # Requisito 2
+# https://app.betrybe.com/course/computer-science/redes-e-raspagem-de-dados/raspagem-de-dados/ab38ab4e-bdbd-4984-8987-1abf32d85f26/conteudos/726e3190-54a2-4c75-9c93-02eaf6f5367b/analisando-respostas/c6601d00-f015-4445-95fe-0c449f8a551c?use_case=side_bar
+# https://devhints.io/css
+# https://devhints.io/xpath
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url_list = selector.css("h2.entry-title a::attr(href)").getall()
+    return url_list
 
 
 # Requisito 3
