5e12c5ece5e2372a98ab04c1147e3e8d5183bb63
Elithon Silva <elithonsilva@gmail.com>
2022-07-01 01:10:12 -0300

    impl func format_date para auxiliar na formatação da data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -15,8 +16,41 @@ def search_by_title(title):
 
 
 # Requisito 7
+# https://docs.python.org/3/library/datetime.html
+def format_date(date):
+    datetime.fromisoformat(date)
+    months = {
+        "01": "janeiro",
+        "02": "fevereiro",
+        "03": "março",
+        "04": "abril",
+        "05": "maio",
+        "06": "junho",
+        "07": "julho",
+        "08": "agosto",
+        "09": "setembro",
+        "10": "outubro",
+        "11": "novembro",
+        "12": "dezembro",
+    }
+
+    year, month = date[0:4], date[5:7]
+    day = date[8:10] if date[8] != "0" else date[9:10]
+
+    date_formated = f"{day} de {months[month]} de {year}"
+
+    return date_formated
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    date_formated = format_date(date)
+    news = search_news({"timestamp": {"$regex": date_formated}})
+    news_list = []
+
+    for one_news in news:
+        news_list.append((one_news["title"], one_news["url"]))
+
+    return news_list
 
 
 # Requisito 8
